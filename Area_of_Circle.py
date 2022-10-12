d2fd3a82c5c3bb9e9283c4fa3cf3830cca83010f
Ericka Jane Alegre <90811936+ErickaJaneAlegre@users.noreply.github.com>
2022-10-12 18:29:18 +0800

    Add files via upload

---
+++ Area_of_Circle.py
@@ -0,0 +1,26 @@
+def main():
+ class Circle:
+  def __init__(self, measurement=1):
+   self._measurement = measurement
+
+
+ class AreaUsingRadius(Circle):
+  def area_radius(self):
+   pi = 3.14
+   return (pi * (self._measurement ** 2))
+
+ radius = float(input("Enter the Radius of the Circle: "))
+ solve = AreaUsingRadius(radius)
+ print("The Area of the Circle is", str(solve.area_radius()) + " square unit", ".")
+
+ class AreaUsingDiameter(Circle):
+  def area_diameter(self):
+   pi = 3.14
+   return pi * ((self._measurement/2) ** 2)
+
+ diameter = float(input("Enter the Diameter of the Circle: "))
+ solve = AreaUsingDiameter(diameter)
+ print("The Area of the Circle is", str(solve.area_diameter()) + " square unit", ".")
+
+
+main()
